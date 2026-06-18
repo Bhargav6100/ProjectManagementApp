cc512c7fc5452b1804f91560d5ed3bde7ed97e90
Bhargav6100 <bhargavfofandi314@gmail.com>
2026-06-18 15:48:59 -0400

    marked entity with table

--- backend/src/main/java/com/ProjectManagementApp/entity/User.java
+++ backend/src/main/java/com/ProjectManagementApp/entity/User.java
@@ -1,10 +1,21 @@
 package com.ProjectManagementApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
+    @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
+    @Column(name = "email", nullable = false, length = 100)
     private String email;
+
 
     User(String firstName, String lastName, String email){
         this.firstName=firstName;
